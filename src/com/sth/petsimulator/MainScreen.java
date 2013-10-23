178af200cf769180eee1371251f0d801c2dc779a
Steven Whitehead <scw007@vt.edu>
2013-10-23 00:26:03 -0400

    Other stuff for initial android project

--- src/com/sth/petsimulator/MainScreen.java
+++ src/com/sth/petsimulator/MainScreen.java
@@ -1,22 +1,12 @@
 package com.sth.petsimulator;
 
+import sofia.app.Screen;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainScreen extends Activity {
+public class MainScreen extends Screen{
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_screen);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_screen, menu);
-		return true;
-	}
 
 }
